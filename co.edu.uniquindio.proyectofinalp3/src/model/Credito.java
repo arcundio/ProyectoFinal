2e75bfff0a90f8acb120e8abb2aa0eb67dc748a3
Usuario <Usuario@DESKTOP-MMLHIU2>
2021-10-22 22:15:57 -0500

    Cambio Credito

--- co.edu.uniquindio.proyectofinalp3/src/model/Credito.java
+++ co.edu.uniquindio.proyectofinalp3/src/model/Credito.java
@@ -1,33 +1,17 @@
 package model;
 
-enum TipoCredito {
-	
-	CULTURAL, DEPORTIVO, ACADEMICO, NO_APLICA;
-	
-	TipoCredito getTipoCredito(String tipoCredito) {
-		if (tipoCredito.equals("Cultural"))
-			return TipoCredito.CULTURAL;
-		if (tipoCredito.equals("Deportivo"))
-			return TipoCredito.DEPORTIVO;
-		if (tipoCredito.equals("Académico"))
-			return TipoCredito.ACADEMICO;
-		
-		return TipoCredito.NO_APLICA;
-	}
-	
-}
 
 public class Credito {
 	
 	private String nombre, lugar;
-	private int codigo, duracion_horas, cupo_maximo;
-	private TipoCredito tipoCredito;
+	private int codigo, duracionHoras, cupoMaximo;
 	private Instructor instructor;
 	
 	
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
@@ -43,23 +27,17 @@ public class Credito {
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
-	public int getDuracion_horas() {
-		return duracion_horas;
+	public int getDuracionHoras() {
+		return duracionHoras;
 	}
-	public void setDuracion_horas(int duracion_horas) {
-		this.duracion_horas = duracion_horas;
+	public void setDuracionHoras(int duracionHoras) {
+		this.duracionHoras = duracionHoras;
 	}
-	public int getCupo_maximo() {
-		return cupo_maximo;
+	public int getCupoMaximo() {
+		return cupoMaximo;
 	}
-	public void setCupo_maximo(int cupo_maximo) {
-		this.cupo_maximo = cupo_maximo;
-	}
-	public TipoCredito getTipoCredito() {
-		return tipoCredito;
-	}
-	public void setTipoCredito(TipoCredito tipoCredito) {
-		this.tipoCredito = tipoCredito;
+	public void setCupoMaximo(int cupoMaximo) {
+		this.cupoMaximo = cupoMaximo;
 	}
 	public Instructor getInstructor() {
 		return instructor;
@@ -67,6 +45,7 @@ public class Credito {
 	public void setInstructor(Instructor instructor) {
 		this.instructor = instructor;
 	}
+
 
 	
 }
